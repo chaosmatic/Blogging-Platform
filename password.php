f12f687bb349a2d9685870b86b02d81862d77458
Jonathan <jonathan.milford@gmail.com>
2012-07-21 20:30:16 +1000

    gave ability to add password

---
+++ password.php
@@ -0,0 +1,28 @@
+<?php 
+require_once('database.php');
+require_once('PasswordHash.php');
+require_once('head.php');
+$dbh = new databaseaccess;
+$hasher = new PasswordHash(8, false);
+$password = $_POST["password"];
+$passwordnew = $_POST["passwordnew"];
+$passwordnew1 = $_POST["passwordnew1"];
+$dbh->gethash();
+$dbh->displayposts(0, 1024); //probably shouldn't hardcode this
+$hash = $dbh->hash;
+$check = $hasher->CheckPassword($password, $hash);
+if ($check) {
+	if ($passwordnew1 == $passwordnew){
+		$newhash = $hasher->HashPassword($passwordnew);
+		if (strlen($hash) < 20){
+			echo "Failed to hash new password";
+		}else{
+			$dbh->writehash($newhash);
+			echo "Password Updated";
+		}
+	}
+}else{
+	echo "Password incorrect";
+}
+require_once('foot.php');
+?>
