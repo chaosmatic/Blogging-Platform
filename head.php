77729f0bb7b842b80937d4cfc47f8f01038b74db
Jonathan <jonathan.milford@gmail.com>
2013-05-18 20:14:46 +1000

    modified for different titles

--- head.php
+++ head.php
@@ -1,11 +1,16 @@
 <?php
+require_once("config.php");
 echo "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'><link rel='shortcut icon' href='favicon.ico'><LINK REL=StyleSheet HREF='style.css' TYPE='text/css' MEDIA=screen><LINK REL=StyleSheet HREF='colour.css' TYPE='text/css' MEDIA=screen>";
-echo "<title>//OfflineBlog</title>"; //title in top of browser
+echo "<title>";
+echo TITLE;
+echo "</title>"; //title in top of browser
 echo "</head>";
 echo "<body>";
 echo "<div id=wrapper>";
 echo "<div id=head><center>";
-echo "<span id='title'><a href='index.php'>//OfflineBlog</a></span>"; //title at top of page
+echo "<span id='title'><a href='index.php'>";
+echo TITLE;
+echo "</a></span>"; //title at top of page
 
 echo "</center>";
 	echo "</div>";
