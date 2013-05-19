6d58635653527825a6a89d97ddfcedd0c6e247ca
Jonathan <jonathan.milford@gmail.com>
2013-05-19 22:00:26 +1000

    updated config file location

--- head.php
+++ head.php
@@ -1,5 +1,5 @@
 <?php
-if (!file_exists('../../config/config.php')){
+if (!file_exists('config.php')){
 	if (stripos($_SERVER["REQUEST_URI"],'setup.php')==false){
 		header('Location: setup.php');
    		exit();
@@ -8,7 +8,7 @@ if (!file_exists('../../config/config.php')){
 	header('Location: index.php');
 	exit();
 }else{
-	require_once("../../config/config.php");
+	require_once("config.php");
 }
 
 echo "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'><link rel='shortcut icon' href='favicon.ico'><LINK REL=StyleSheet HREF='style.css' TYPE='text/css' MEDIA=screen><LINK REL=StyleSheet HREF='colour.css' TYPE='text/css' MEDIA=screen>";
