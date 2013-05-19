2982dc2b250c27e9d63c2ee84c5689c718233954
Jonathan <jonathan.milford@gmail.com>
2013-05-19 22:38:16 +1000

    integrated old website

--- head.php
+++ head.php
@@ -11,19 +11,27 @@ if (!file_exists('config.php')){
 	require_once("config.php");
 }
 
-echo "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'><link rel='shortcut icon' href='favicon.ico'><LINK REL=StyleSheet HREF='style.css' TYPE='text/css' MEDIA=screen><LINK REL=StyleSheet HREF='colour.css' TYPE='text/css' MEDIA=screen>";
+echo "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'><link rel='shortcut icon' href='".$dir."favicon.ico'><LINK REL=StyleSheet HREF='".$dir."style.css' TYPE='text/css' MEDIA=screen><LINK REL=StyleSheet HREF='".$dir."colour.css' TYPE='text/css' MEDIA=screen>";
 echo "<title>";
-echo TITLE;
+echo $title.TITLE;
 echo "</title>"; //title in top of browser
 echo "</head>";
 echo "<body>";
 echo "<div id=wrapper>";
 echo "<div id=head><center>";
-echo "<span id='title'><a href='index.php'>";
+echo "<span id='title'><a href='".$dir."index.php'>";
 echo TITLE;
 echo "</a></span>"; //title at top of page
 
 echo "</center>";
+echo "<ul id='list-nav'>";
+ //these are the links at the top of page. between the > < is the appearance, in the ' ' is the location
+echo "<li><a href='".$dir."code/index.php'>Code</a> </li>";
+echo "<li><a href='".$dir."minecraft/index.php'>Minecraft</a> </li>";
+echo "<li><a href='".$dir."documents/index.php'>Documents</a></li>";
+echo "<li><a href='".$dir."contact/index.php'>Contact</a> </li>";
+echo "</ul><br>";
 	echo "</div>";
+
 	echo "<div id=content>";
 ?>
