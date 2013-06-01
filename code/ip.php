b5d71a8f7e974f67d088d3f1a866f3cd373e2394
Jonathan <jonathan.milford@gmail.com>
2013-06-01 20:09:14 +1000

    htmlspecialchar.d

--- code/ip.php
+++ code/ip.php
@@ -7,10 +7,10 @@ echo " </h1>";
 ?>
 <pre>
 &lt;?php
-echo "<title>Your Ip</title> <h1> Your IP Address is: ";
+echo "&lt;title&gt;Your Ip&lt;/title&gt; &lt;h1&gt; Your IP Address is: ";
 $ip =  filter_var($_SERVER['REMOTE_ADDR'],FILTER_VALIDATE_IP);
 echo $ip;
-echo " </h1>";
+echo " &lt;/h1&gt;";
 
 ?&gt;
 </pre>
