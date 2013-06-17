b3a860b11b173b030a0364c2073521623f6f84c2
Jonathan <jonathan.milford@gmail.com>
2013-06-17 17:55:20 +1000

    added piwik tracking code

--- foot.php
+++ foot.php
@@ -1,5 +1,23 @@
 <?php
 echo "</div>"; 
 echo "</div>";
+?>
+<!-- Piwik -->
+<script type="text/javascript"> 
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://192.168.1.5/web/piwik//";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+    g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+
+</script>
+<noscript><p><img src="http://192.168.1.5/web/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+<?php
 echo "</body>";
 ?>
